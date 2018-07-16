7cc063afd15ed66647949f792a6928e4c48a84ab
Oscar Zambrano <oscarz.developer@gmail.com>
2018-07-16 14:05:34 -0500

    Adding first next.js page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HelloWorld () {
+  return (
+    <div>
+      <h1>{'Hola Oscar!'}</h1>
+      <p>{'Bienvenido al curso de Next.js'}</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
