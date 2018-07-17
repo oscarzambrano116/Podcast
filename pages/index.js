b0dda14de211f8cd8591406e7d71a2668cbe8b95
Oscar Zambrano <oscarz.developer@gmail.com>
2018-07-17 18:15:59 -0500

    Adding api call to Index page

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,73 @@
-import React from 'react';
+import 'isomorphic-fetch';
+import React, { Component } from 'react';
 
-function HelloWorld () {
-  return (
-    <div>
-      <h1>{'Hola Oscar!'}</h1>
-      <p>{'Bienvenido al curso de Next.js'}</p>
-      <img src="/static/platzi-logo.png" alt="Platzi logo"/>
-      <style jsx>
-        {`
-          h1 {
-            color: red;
+class Index extends Component {
+  static async getInitialProps() {
+    const req = await fetch('https://api.audioboom.com/channels/recommended');
+    const { body: channels } = await req.json();
+
+    return { channels }
+  }
+
+  render () {
+    const { channels } = this.props;
+    return (
+      <div>
+        <header>{'Podcast'}</header>
+        <div className="channels">
+          {
+            channels.map((channel) => (
+              <div key={channel.id} className={'channel'}>
+                <img src={channel.urls.logo_image.original} alt="Logo"/>
+                <h2>{channel.title}</h2>
+              </div>
+            ))
           }
-          p {
-            color: green;
-          }
-          img {
-            max-width: 50%;
-            display: block;
-            margin: 0 auto;
-          }
-        `}
-      </style>
-      <style jsx global>
-        {`
-          body {
-            background: white;
-          }
-        `}
-      </style>
-    </div>
-  );
+        </div>
+        <style jsx>
+          {`
+            header {
+              color: #FFF;
+              background: #8756CA;
+              padding: 15px;
+              text-align: center;
+            }
+            .channels {
+              display: grid;
+              grid-gap: 15px;
+              padding: 15px;
+              grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+            }
+            .channel {
+              display: block;
+              border-radius: 3px;
+              box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+              margin-bottom: 0.5em;
+            }
+            .channel img {
+              width: 100%;
+            }
+            h2 {
+              padding: 5px;
+              font-size: 0.9em;
+              font-weight: 600;
+              margin: 0;
+              text-align: center;
+            }
+          `}
+        </style>
+        <style jsx global>
+          {`
+            body {
+              margin: 0;
+              font-family: system-ui;
+              background: white;
+            }
+          `}
+        </style>
+      </div>
+    );
+  }
 }
 
-export default HelloWorld;
+export default Index;
