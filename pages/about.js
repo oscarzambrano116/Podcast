cb19d39a47ac7f1ac4dd11af91526348537f218a
Oscar Zambrano <oscarz.developer@gmail.com>
2018-07-16 20:37:11 -0500

    Adding about page

---
+++ pages/about.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className={'container'}>
+      <img
+        src={'https://static.platzi.com/media/achievements/badge-reactnative-9c23a814-e9c3-4041-afbd-ff8083fbf32f.png'}
+        alt={'React logo'}
+      />
+      <div>
+        <h2>{'Oscar Zambrano'}</h2>
+        <p>{'curso de next.js'}</p>
+      </div>
+      <style jsx>
+        {`
+          .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+            height: 100%;
+          }
+          h2 {
+            color: white;
+          }
+          p {
+            color: lightblue;
+            text-align: center;
+          }
+          img {
+            max-width: 150px;
+            display: block;
+          }
+        `}
+      </style>
+      <style jsx global>
+        {`
+          body {
+            background-color: #022c43;
+          }
+          #__next, body, html {
+            height: 100%;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,29 @@ function HelloWorld () {
     <div>
       <h1>{'Hola Oscar!'}</h1>
       <p>{'Bienvenido al curso de Next.js'}</p>
+      <img src="/static/platzi-logo.png" alt="Platzi logo"/>
+      <style jsx>
+        {`
+          h1 {
+            color: red;
+          }
+          p {
+            color: green;
+          }
+          img {
+            max-width: 50%;
+            display: block;
+            margin: 0 auto;
+          }
+        `}
+      </style>
+      <style jsx global>
+        {`
+          body {
+            background: white;
+          }
+        `}
+      </style>
     </div>
   );
 }
